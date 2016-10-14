5c8f30511a52e063495ca5c03c88eaca5c181c5b
Marshall Reisdorph <mkreisdorph@gmail.com>
2016-10-13 21:19:25 -0500

    update AS if problems, added Instruction class and code style

---
+++ app/src/main/java/com/example/marshall/gouldensays/Instruction.java
@@ -0,0 +1,14 @@
+package com.example.marshall.gouldensays;
+
+/**
+ * Created by Marshall on 10/13/2016.
+ */
+
+public class Instruction {
+
+    //compares user input to the instruction that was given at this stage
+    public boolean inputMatchesInstruction()
+    {
+        return true;
+    }
+}
