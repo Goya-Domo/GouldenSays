a61ee724fb4d80f7cc15efc1f210bd639fe10b73
Marshall Reisdorph <mkreisdorph@gmail.com>
2016-10-15 14:46:46 -0500

    fleshing out Instruction class. Game class added but may be temporary

---
+++ app/src/main/java/com/example/marshall/gouldensays/Game.java
@@ -0,0 +1,37 @@
+package com.example.marshall.gouldensays;
+
+import java.util.Random;
+
+/**
+ * Created by Marshall on 10/15/2016.
+ *
+ * The whole point of managing the seed of the Random device is to be able to replicate games while also having RNG.
+ * Hopefully that will work.
+ *
+ */
+
+public class Game
+{
+    private long seed;
+
+    private Random rng;
+    public Game()
+    {
+        //create a random seed and save it.
+        rng = new Random();
+        seed = rng.nextLong();
+        rng.setSeed(seed);
+    }
+
+    public Game(long seed)
+    {
+        this.seed = seed;
+        rng = new Random(seed);
+    }
+
+    public Random getRng()
+    {
+        return rng;
+    }
+
+}
--- app/src/main/java/com/example/marshall/gouldensays/Instruction.java
+++ app/src/main/java/com/example/marshall/gouldensays/Instruction.java
@@ -6,9 +6,47 @@ package com.example.marshall.gouldensays;
 
 public class Instruction {
 
+    private int numButtons;
+
+    //Using object of type Integer instead of int because Integer is nullable (We may have instructions that don't correspond to a particular button)
+    private Integer button;
+    private Game game;
+
+    public Instruction(Game game)
+    {
+        numButtons = 4;
+        this.game = game;
+        button = getRandButton();
+    }
+
+    public Instruction(Game game, int numButtons)
+    {
+        this.numButtons = numButtons;
+        this.game = game;
+        button = getRandButton();
+    }
+
+
     //compares user input to the instruction that was given at this stage
     public boolean inputMatchesInstruction()
     {
         return true;
     }
+
+    public int getButton()
+    {
+        return button;
+    }
+
+    //Choose a random button
+    private int getRandButton()
+    {
+        int butt = game.getRng().nextInt() % 10;
+
+        if (butt >= numButtons)
+            butt = getRandButton();
+
+        return butt;
+    }
+
 }
