552bc50d35a28da7f0cbbebabd9ea5b6ab84272f
Marshall Reisdorph <mkreisdorph@gmail.com>
2016-10-26 18:26:28 -0500

    ***moved hexes, functionality WIP*** * Added integer values to resources xml file and gave integer tags to each button * Started some game functionality, it exits to menu when fails (which is always right now) * updated App name string resource

--- app/src/main/java/com/example/marshall/gouldensays/GameActivity.java
+++ app/src/main/java/com/example/marshall/gouldensays/GameActivity.java
@@ -8,6 +8,8 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -16,6 +18,7 @@ public class GameActivity extends AppCompatActivity
 {
     private int instIndex;
     private int responseIndex;
+    private boolean animating;
     private Game game;
     private ArrayList<Instruction> instructionList;
     //private ArrayList<View> images;
@@ -26,16 +29,12 @@ public class GameActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_layout);
 
-       /* images.add(findViewById(R.id.yellow_hex));
-        images.add(findViewById(R.id.red_hex));
-        images.add(findViewById(R.id.green_hex));
-        images.add(findViewById(R.id.blue_hex));*/
-
         game = new Game();
         instructionList = new ArrayList<>();
         instIndex = 0;
         responseIndex = 0;
 
+        addNextInstruction();
         addNextInstruction();
         playNextInstruction();
     }
@@ -45,33 +44,40 @@ public class GameActivity extends AppCompatActivity
         if (instIndex >= instructionList.size())
         {
             instIndex = 0;
+            animating = false;
             return;
         }
+        animating = true;
 
-        Animation shake = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.buttonsig);
-
-        Toast.makeText(getApplicationContext(), String.valueOf(instructionList.get(instIndex).getButton()), Toast.LENGTH_SHORT).show();
-
+        //Rotate to display instruction
+        RotateAnimation ra = null;
         switch ((instructionList.get(instIndex)).getButton())
         {
-            case 0: findViewById(R.id.red_hex).startAnimation(shake);
+            case 0: ra = rotation(findViewById(R.id.yellow_hex), true, 6);
                 break;
-            case 1: findViewById(R.id.blue_hex).startAnimation(shake);
+            case 1: ra = rotation(findViewById(R.id.red_hex), true, 6);
                 break;
-            case 2: findViewById(R.id.green_hex).startAnimation(shake);
+            case 2: ra = rotation(findViewById(R.id.blue_hex), true, 6);
                 break;
-            case 3: findViewById(R.id.yellow_hex).startAnimation(shake);
+            case 3: ra = rotation(findViewById(R.id.green_hex), true, 6);
         }
 
-        shake.setAnimationListener(new Animation.AnimationListener(){
-            public void onAnimationStart(Animation a){}
-            public void onAnimationRepeat(Animation a){}
-            public void onAnimationEnd(Animation a){
-                instIndex++;
-                playNextInstruction();
-            }
-            });
+        //When done rotating, rotate the next button
+        if (ra != null)
+        {
+            ra.setAnimationListener(new Animation.AnimationListener()
+            {
+                public void onAnimationStart(Animation a) {}
 
+                public void onAnimationRepeat(Animation a) {}
+
+                public void onAnimationEnd(Animation a)
+                {
+                    instIndex++;
+                    playNextInstruction();
+                }
+            });
+        }
     }
 
     private void addNextInstruction()
@@ -88,7 +94,41 @@ public class GameActivity extends AppCompatActivity
 
     public void hexClick(View view)
     {
-        rotation(view, 1);
+        if (!animating)
+        {
+            rotation(view, true);
+            respond(view);
+        }
+    }
+
+    private boolean respond(View view)
+    {
+        boolean correct = false;
+        if (view.getTag() instanceof Integer)
+        {
+            Integer hexNum = (Integer)view.getTag();
+
+            if (instructionList.get(responseIndex).equals(hexNum))
+            {
+                correct = true;
+                if (++responseIndex == instructionList.size())
+                {
+                    responseIndex = 0;
+                    addNextInstruction();
+                    playNextInstruction();
+                }
+            }
+            else
+            {
+                exitToMenu(view);
+            }
+        }
+        else
+        {
+            exitToMenu(view);
+        }
+
+        return correct;
     }
 
     /*public void addButton5()
@@ -104,10 +144,25 @@ public class GameActivity extends AppCompatActivity
 
     }*/
 
-    public void rotation(View view, int direction)
+    public void debugAddHex(View view)
+    {
+        if (findViewById(R.id.black_hex).getVisibility() == View.INVISIBLE)
+        {
+            findViewById(R.id.black_hex).setVisibility(View.VISIBLE);
+        }
+        else
+        {
+            if (findViewById(R.id.white_hex).getVisibility() == View.INVISIBLE)
+            {
+                findViewById(R.id.white_hex).setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    public void rotation(View view, boolean clockwise)
     {
         RotateAnimation anim;
-        if(direction > 1)
+        if(clockwise)
         {
             anim = new RotateAnimation(0.0f, 60.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         }
@@ -120,6 +175,25 @@ public class GameActivity extends AppCompatActivity
         anim.setDuration(200); //Put desired duration per anim cycle here, in milliseconds
 
         view.startAnimation(anim);
+    }
+
+    public RotateAnimation rotation(View view, boolean clockwise, int nTimes)
+    {
+        RotateAnimation anim;
+        if(clockwise)
+        {
+            anim = new RotateAnimation(0.0f, (float)(60.0 * nTimes), Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        }
+        else
+        {
+            anim = new RotateAnimation((float)(nTimes * 60.0), 0.0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        }
+        anim.setInterpolator(new LinearInterpolator());
+
+        anim.setDuration(200 * nTimes); //Put desired duration per anim cycle here, in milliseconds
+
+        view.startAnimation(anim);
+        return anim;
     }
 
 
